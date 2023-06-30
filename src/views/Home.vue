214629aa4f8d3b510f4c4c03e0ec1369d87e586d
Hillen, J.D <jd.hillen@garmin.com>
2023-06-30 13:34:04 -0500

    Update - Hours and Map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,38 @@
 <template>
   <MetaData :data="metaData" />
   <section class="home">
-    <h1>Coming Soon!</h1>
+    <hr>
     <div class="row">
       <div class="twelve columns">
-        <img class="img" alt="" src="@/assets/img/building.jpg" />
+        <h5>A New Website is Coming Soon!</h5>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="twelve columns">
+        <img class="img" alt="" src="@/assets/img/building.png" />
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="six columns">
+        <h3>Hours</h3>
+        <ul>
+          <li><strong>Thursday</strong>: 4pm - 10pm</li>
+          <li><strong>Friday</strong>: 4pm - 10pm</li>
+          <li><strong>Saturday</strong>: 12pm - 10pm</li>
+          <li><strong>Sunday</strong>: 12pm - 7pm</li>
+        </ul>
+      </div>
+      <div class="six columns">
+        <h3>Directions</h3>
+        <a href="https://goo.gl/maps/vq3EUog1hr7N5xY86" target="_blank" rel="noopener noreferrer">11018 Johnson Dr<br/>Shawnee, KS 66203</a>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="twelve columns">
+        <footer>Copyright © 2023 - Friction Brewing Company</footer>
       </div>
     </div>
   </section>
@@ -28,7 +56,7 @@ export default {
     return {
       metaData: {
         title: 'Home',
-        description: 'This is the description for the Home page',
+        description: 'A music soaked beer experience featuring an ever-evolving array of fantastically-flavorful, hoppy, stouty beers.',
         image: 'https://via.placeholder.com/1500x780'
       }
     };
@@ -62,5 +90,16 @@ export default {
 <style lang="scss" scoped>
 .home {
   text-align: center;
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  footer {
+    font-size: 12px;
+    margin-bottom: 50px;
+  }
 }
 </style>
